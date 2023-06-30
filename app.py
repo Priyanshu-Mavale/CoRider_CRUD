a879bcee8d866cfcf943311fa662e2e74d6349c5
Priyanshu-Mavale <priyanshumavale@gmail.com>
2023-07-01 00:28:33 +0530

    done changes

--- app.py
+++ app.py
@@ -3,114 +3,68 @@ from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import jsonify,request
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_restful import Resource,Api,reqparse,abort
 from flask_pymongo import PyMongo
 
 app= Flask(__name__)
 app.secret_key="secretKey"
+
+api=Api(app)
 app.config["MONGO_URI"]="mongodb://localhost:27017/CoRiderDatabase"
 mongo=PyMongo(app)
 
-# @app.route('/add',methods=['POST'])
-# def add_user():
-#     _json=request.json
-#     _name=_json['name']
-#     _email=_json['email']
-#     _password=_json['password']
+errors = {
+    'NotFoundError': {
+        'message':'Not Found',
+        'status': 404,
+    },
+}
+api=Api(app,errors=errors)
 
-#     if _name and _email and _password and request.method=='POST':
-#         _hashed_password=generate_password_hash(_password)
 
-#         id=mongo.db.Users.insert_one({'name':_name,'email':_email,'password':_hashed_password})
-#         resp=jsonify("User added Successfully")
-#         resp.status_code=200
-#         return resp
-#     else:
-#         return not_found()
+class Users(Resource):
+    def get(self):
+        users=mongo.db.Users.find()
+        resp=dumps(users)
+        return resp
     
-@app.route('/users',methods=['POST','GET'])
-def users():
-    if request.method=='POST':
+    def post(self):
         _json=request.json
         _name=_json['name']
         _email=_json['email']
         _password=_json['password']
-
-        if _name and _email and _password and request.method=='POST':
+        if _name and _email and _password:
             _hashed_password=generate_password_hash(_password)
-
             id=mongo.db.Users.insert_one({'name':_name,'email':_email,'password':_hashed_password})
             resp=jsonify("User added Successfully")
             resp.status_code=200
             return resp
-        else:
-            return not_found()     
-    elif request.method=='GET':
-        users=mongo.db.Users.find()
-        resp=dumps(users)
-        return resp
-
-@app.route('/users/<id>',methods=['GET','PUT','DELETE'])
-def user(id):
-    if request.method=='GET':
+        
+class User(Resource):
+    def get(self,id):
         users=mongo.db.Users.find_one({'_id':ObjectId(id)})
         resp=dumps(users)
         return resp
-    elif request.method=='PUT':
+    def put(self,id):
         _id=id
         _json=request.json
         _name=_json['name']
         _email=_json['email']
         _password=_json['password']
-
-        if _name and _email and _password and _id and request.method=='PUT':
+        if _name and _email and _password and _id:
             _hashed_password=generate_password_hash(_password)
             mongo.db.Users.update_one({'_id':ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},{'$set':{'name':_name,'email':_email,'password':_hashed_password}})
             resp=jsonify("User updates successfully")
             resp.status_code=200
             return resp
-        else:
-            return not_found()
-    elif request.method=='DELETE':
+    def delete(self,id):
         mongo.db.Users.delete_one({'_id':ObjectId(id)})
         resp=jsonify("User deleted successfully")
         resp.status_code=200
-        return resp
+        return resp        
 
-# @app.route("/delete/<id>",methods=['DELETE'])
-# def delete_user(id):
-#     mongo.db.Users.delete_one({'_id':ObjectId(id)})
-#     resp=jsonify("User deleted successfully")
-#     resp.status_code=200
-#     return resp
-
-# @app.route("/update/<id>",methods=['PUT'])
-# def update_user(id):
-#     _id=id
-#     _json=request.json
-#     _name=_json['name']
-#     _email=_json['email']
-#     _password=_json['password']
-
-#     if _name and _email and _password and _id and request.method=='PUT':
-#         _hashed_password=generate_password_hash(_password)
-#         mongo.db.Users.update_one({'_id':ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},{'$set':{'name':_name,'email':_email,'password':_hashed_password}})
-#         resp=jsonify("User updates successfully")
-#         resp.status_code=200
-#         return resp
-#     else:
-#         return not_found()
-
-
-@app.errorhandler(404)
-def not_found(error=None):
-    message={
-        'status':404,
-        'message':'Not Found'+request.url
-    }
-    resp=jsonify(message)
-    resp.status_code=404
-    return resp
-
+api.add_resource(Users,'/users')
+api.add_resource(User,'/users/<id>')
 
 if __name__=="__main__":
     app.run(debug=True)
